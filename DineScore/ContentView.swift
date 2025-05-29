dd405c242b1c703edf7324e4d419cdf8030f8783
fromo0214 <faromo0214@gmail.com>
2025-05-28 20:17:51 -0700

    Initial Commit

---
+++ DineScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DineScore
+//
+//  Created by Fernando Romo on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DineScore/DineScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineScoreApp.swift
+//  DineScore
+//
+//  Created by Fernando Romo on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DineScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
